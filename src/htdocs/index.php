4196b00b91656f097c7ff3958908de867e165393
Eric Martinez <emartinez@usgs.gov>
2013-10-27 09:30:20 -0600

    Switched to using index.php rather than index.html.

---
+++ src/htdocs/index.php
@@ -0,0 +1,33 @@
+<?php
+// Hack to detect server installation
+if (function_exists('param')) {
+	// Server install...
+	$TITLE = 'Risk Targeted Ground Motion Calculator';
+	$STYLESHEETS = 'css/index.css';
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/template/template.inc.php';
+} else {
+	// Local install...
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Risk Targeted Ground Motion Calculator</title>
+	<link rel="stylesheet" href="css/index.css"/>
+</head>
+<body>
+	<h1>Risk Targetted Ground Motion Calculator</h1>
+<?php
+}
+?>
+
+<p>
+	Web application for comuting risk-targeted ground motion values.
+</p>
+
+<div id="application"></div>
+<script src="requirejs/require.js" data-main="js/index.js"></script>
+
+<?php if (function_exists('param')) : ?>
+</body>
+</html>
+<?php endif; ?>
