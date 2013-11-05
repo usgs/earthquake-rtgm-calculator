c155e82affebc1d05e1c6151d95e153fd317e727
Jonathan Brown <jdbrown@usgs.gov>
2013-11-05 13:24:26 -0700

    added error messages

--- src/htdocs/js/rtgm/Curve.js
+++ src/htdocs/js/rtgm/Curve.js
@@ -13,20 +13,32 @@ define([
 
 	var Curve = function (attributes) {
 		var i = null, len = null;
-
 		// check the array sizes
 		if (!attributes || (attributes.xs.length !== attributes.ys.length)) {
-			throw 'X and Y arrays (of the same size) are required';
+			throw 'X and Y arrays (of the same size) are required.';
 		}
-
+		// check if x and y arrays are less than 2 values
+		if ((attributes.xs.length || attributes.ys.length) < 2){
+			throw 'X and Y arrays require at least 2 values.';
+		}
+		//Check X array for empty element		
+		for (i = 0; i < attributes.xs.length; i++){
+                        if (attributes.xs[i] === ''){
+                                throw 'X values require a numerical value after each comma.';
+                        }
+                }
+		//Check Y array for empty element
+		for (i = 0; i < attributes.ys.length; i++){
+			if (attributes.ys[i] === ''){
+				throw 'Y values require a numerical value after each comma.';
+			}
+		}
 		for (i = 0, len = attributes.xs.length; i < len; i++) {
 			attributes.xs[i] = parseFloat(attributes.xs[i]);
 			attributes.ys[i] = parseFloat(attributes.ys[i]);
 		}
-
 		// Call parent constructor
 		Model.call(this, Util.extend({}, DEFAULTS, attributes));
-
 	};
 
 	Curve.prototype = Object.create(Model.prototype);
