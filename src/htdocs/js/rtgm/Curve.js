cb3435b519875d26f1ac0ea96c5f3fe118c3f773
Jonathan Brown <jdbrown@usgs.gov>
2013-11-13 15:35:23 -0700

    Removed for loop

--- src/htdocs/js/rtgm/Curve.js
+++ src/htdocs/js/rtgm/Curve.js
@@ -12,7 +12,7 @@ define([
 	};
 
 	var Curve = function (attributes) {
-		var i = null;//, len = null;
+		var i = null;
 		// Check that array sizes are the same size
 		if (!attributes || (attributes.xs.length !== attributes.ys.length)) {
 			throw 'X and Y arrays (of the same size) are required.';
@@ -21,19 +21,13 @@ define([
 		if ((attributes.xs.length || attributes.ys.length) < 2){
 			throw 'X and Y arrays require at least 2 values.';
 		}
-		//Check if X and Y are numerical values
+		//Check X and Y values for numerical values
 		for (i = 0; i < attributes.ys.length; i++){
 			if (isNaN(attributes.xs[i]) || isNaN(attributes.ys[i])){
 				throw 'X and Y values must be numerical.';
 			}
 		}
-		/*I am not so sure that this for loop is needed. 
-		*for (i = 0, len = attributes.xs.length; i < len; i++) {
-		*	attributes.xs[i] = parseFloat(attributes.xs[i]);
-		*	attributes.ys[i] = parseFloat(attributes.ys[i]);
-		*}
-		* Call parent constructor
-		*/
+		// Call parent constructor
 		Model.call(this, Util.extend({}, DEFAULTS, attributes));
 	};
 
