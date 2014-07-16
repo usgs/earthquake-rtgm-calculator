8f6397dfe49ef5800f1a10caacd5751a07f8e0fb
Eric Martinez <emartinez@usgs.gov>
2014-07-16 16:10:45 -0600

    Added intro text.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -20,9 +20,12 @@ if (!isset($TEMPLATE)) {
 ?>
 
 <p class="intro">
-	Web application for computing risk-targeted ground motion values. For help
-	using this tool, or for guidance on programmatic data access, please read
-	<a href="documentation.php">the documentation</a>.
+	This web application can be used to calculate risk-targeted ground motion
+	values in accordance with &ldquo;Method 2&rdquo; of 2010 ASCE 7 Standard
+	Section 21.2.1.2. For help using this tool, or for guidance on programmatic
+	data access, <a href="https://github.com/usgs/earthquake-rtgm-
+calculator/wiki/Risk-Targeted-Ground-Motion-Calculator-Documentation">please
+	read the Documentation</a>.
 </p>
 
 <div id="application"></div>
